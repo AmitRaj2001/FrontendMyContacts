36c0fc5443d2b40d7795ba9b8be4599a9681e8b4
AmitRaj2001 <amit.12018323@lpu.in>
2023-08-16 02:34:48 +0530

    modified:   src/App.js 	modified:   src/Components/Card.js 	modified:   src/Components/Footer.js 	modified:   src/Components/Navbar.js 	modified:   src/Screens/Aboutus.js 	modified:   src/Screens/Contactus.js 	modified:   src/Screens/Homepage.js 	modified:   src/Screens/Login.js 	modified:   src/Screens/Signup.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
-import './App.css';
-import Homepage from './Screens/Homepage';
-import Login from './Screens/Login';
-import Signup from './Screens/Signup';
-import Aboutus from './Screens/Aboutus';
-import Contactus from './Screens/Contactus';
-
-
-
+import "./App.css";
+import Homepage from "./Screens/Homepage";
+import Login from "./Screens/Login";
+import Signup from "./Screens/Signup";
+import Aboutus from "./Screens/Aboutus";
+import Contactus from "./Screens/Contactus";
 
 // bootstrap
-import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
-
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-   
-    <div className="App">
-      <Routes>
-        <Route exact path='/' element = {<Homepage/>}/> 
-        <Route exact path='/aboutus' element = {<Aboutus/>}/> 
-        <Route exact path='/contactus' element = {<Contactus/>}/>  
-        <Route exact path='/login' element = {<Login/>}/>  
-        <Route exact path='/signup' element = {<Signup/>}/> 
-  
-      </Routes>
-      
-    </div>
-    
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route exact path="/" element={<Homepage />} />
+                <Route exact path="/aboutus" element={<Aboutus />} />
+                <Route exact path="/contactus" element={<Contactus />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/signup" element={<Signup />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
